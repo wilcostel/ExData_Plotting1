5301d5c0a47021c6008412a7c9499c2b7da7ceba
wilcostel <wilcostel@hotmail.com>
2015-06-07 19:43:41 +0200

    Fourth commit

--- Plot3.R
+++ Plot3.R
@@ -31,12 +31,10 @@ with(data, {
 })
 
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       
-       
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 # Copying the plot
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=600, width=600)
 dev.off()
 
